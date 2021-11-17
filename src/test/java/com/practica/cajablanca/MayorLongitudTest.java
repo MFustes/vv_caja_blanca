907645247307844e9b89b7264ff3a4b39323aee7
juancastanog <juan.castanog@alumnos.upm.es>
2021-11-17 20:58:47 +0100

    Fixed MayorLongitud

--- src/test/java/com/practica/cajablanca/MayorLongitudTest.java
+++ src/test/java/com/practica/cajablanca/MayorLongitudTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MayorLongitudTest {
 
-  //Hola chicos, os habla el Mario del pasado, suerte con los tests ;)
+    //Hola chicos, os habla el Mario del pasado, suerte con los tests 😉
     @Test
     public void emptyMayorLongitud() throws EmptyCollectionException {  // 1-2-3-16
         Editor editor= new Editor();
@@ -15,7 +15,7 @@ public class MayorLongitudTest {
     }
 
     @Test
-    public void MayorLongitud() throws EmptyCollectionException {  // 1-2-3-4-5-6-7-8-9-6-4-16
+    public void singleMayorLongitud() throws EmptyCollectionException {  // 1-2-3-4-5-6-7-8-9-6-4-16
         Editor editor= new Editor();
         editor.leerFichero("src/test/java/com/practica/cajablanca/fichero1.txt");
         assertEquals("patata",editor.mayorLongitud());
