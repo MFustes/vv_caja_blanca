9c46b3a14ced60409c75f4f8c1c4a9f383792662
juancastanog <juan.castanog@alumnos.upm.es>
2021-11-18 20:10:14 +0100

    A

--- src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
+++ src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
@@ -1,5 +1,13 @@
 package com.practica.cajablanca;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SustituirPalabraTest {
     //Test de Ines y Dani
+    @Test
+    public void SustituirPalabraTest5() {
+        assertEquals("[A, B, C, " + "]", " ");
+    }
 }
