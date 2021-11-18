2e82a3f27cfd7b7528cfcb52327d703987978667
inesromeroreboto <ines.romero.reboto@alumnos.upm.es>
2021-11-18 18:59:12 +0100

    sustituirPalabra1

--- src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
+++ src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
@@ -1,5 +1,31 @@
 package com.practica.cajablanca;
 
+import com.cajanegra.AbstractSingleLinkedListImpl;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.assertEquals;
+
 public class SustituirPalabraTest {
     //Test de Ines y Dani
+    @BeforeEach
+    public static void leerDatos(){
+        editor.leerFichero("src/test/java/com/practica/cajablanca/fichero1.txt");
+    }
+    public static Editor editor= new Editor();
+    @Test
+    public void sustituirPalabraCamino1(){ //1-15
+        Editor editor= new Editor();
+        assertEquals(0,editor.size());
+    }
+
+    @Test
+
+    public void sustituirPalabraCamino2() {
+        Editor editor= new Editor();
+        AbstractSingleLinkedListImpl<String> lista= this.editor.getLinea(1);
+        assertEquals(" ",lista.getAtPos());
+    }
+
+
 }
