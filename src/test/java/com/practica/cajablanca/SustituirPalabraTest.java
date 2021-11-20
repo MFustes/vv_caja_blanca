3ea0659c1d813aabe3b0a4e26781cfdfe4a4c221
MFustes <m.fustes@alumnos.upm.es>
2021-11-20 12:56:01 +0100

    Final Commit

--- src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
+++ src/test/java/com/practica/cajablanca/SustituirPalabraTest.java
@@ -10,45 +10,45 @@ import static org.junit.Assert.assertThrows;
 
 public class SustituirPalabraTest {
     //Test de Ines y Dani
-
-    @Test
-    public void sustituirPalabraCamino1() { //1-15
-        Editor editor = new Editor();
-        assertEquals(true, editor.editIsEmpty());
+    Editor editor = new Editor();
+    @BeforeEach
+    public  void leerDatos(){
+        editor = new Editor();
     }
 
     @Test
-
-    public void sustituirPalabraCamino2() { //1-2-3-4-5-12-13-14-15
-        Editor editor = new Editor();
-        editor.leerFichero("src/test/java/com/practica/cajablanca/fichero3.txt");
-        //assertEquals(false, editor.editIsEmpty());
-        editor.sustituirPalabra("hola", "dani");
+    public void sustituirPalabraCamino1() { //1-2-22
+        editor.sustituirPalabra("","");
+        assertEquals(0, editor.size());
+    }
+    //Camino no factible
+    @Test
+    public void sustituirPalabraCamino2() { //1-2-3-4-5-6-7-8-9-17-18-19-20-22
     }
 
+    // No encuentra la palabra en el fichero
     @Test
-    public void sustituirPalabrasCamino3() throws EmptyCollectionException { //1-2-3-4-5-6-7-9-10-11-5-12-13-14-15
-        Editor editor = new Editor();
+    public void sustituirPalabrasCamino3() throws EmptyCollectionException { // 1-2-3-4-5-6-7-8-9-10-12-13-15-9-17-18-19-20-22
         editor.leerFichero("src/test/java/com/practica/cajablanca/fichero2.txt");
-        editor.sustituirPalabra("palabra", "hola");
-
+        editor.sustituirPalabra("hola", "flores");
+        assertEquals("[palabra]", editor.getLinea(1).toString());
     }
 
+    //Se encuentra la palabra
     @Test
-    public void sustituirPalabrasCamino4() { //1-2-3-4-5-6-8-9-10-11-5-12-13-14-15
-        Editor editor = new Editor();
+    public void sustituirPalabrasCamino4() throws EmptyCollectionException { //1-2-3-4-5-6-7-8-9-10-11-15-9-17-18-19-20-22
         editor.leerFichero("src/test/java/com/practica/cajablanca/fichero2.txt");
-        editor.sustituirPalabra("flores", "hola");
-
+        editor.sustituirPalabra("palabra", "flores");
+        assertEquals("[flores]", editor.getLinea(1).toString());
     }
 
+
     @Test
-    public void sustituirPalabrasCamino5() throws EmptyCollectionException { //1-2-3-4-5-6-8-9-10-11-5-12-13-3-4-5-12-13-14-15
-        Editor editor = new Editor();
+    public void sustituirPalabrasCamino5() throws EmptyCollectionException { //1-2-3-4-5-6-8-9-10-11-15-9-17-18-19-5-6-7-8-9-10-11-15-9-17-18-19-20-22
         editor.leerFichero("src/test/java/com/practica/cajablanca/fichero4.txt");
-        editor.sustituirPalabra("flores", "hola");
+        editor.sustituirPalabra("buenas", "buenisimas");
+        assertEquals("[Hola, buenisimas]", editor.getLinea(1).toString());
     }
-
 
 
 
